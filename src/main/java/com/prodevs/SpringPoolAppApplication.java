ae928104c7ee7b5f393af08dfd98ae182d3538a5
K0XM4N <radwan.krzysztof.1993@gmail.com>
2017-05-12 22:58:54 +0200

    Project seed added to repo

---
+++ src/main/java/com/prodevs/SpringPoolAppApplication.java
@@ -0,0 +1,12 @@
+package com.prodevs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPoolAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPoolAppApplication.class, args);
+	}
+}
