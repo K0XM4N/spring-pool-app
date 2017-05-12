d9ddc7cc87652f260660358bcd2acbfca2c7afa6
K0XM4N <radwan.krzysztof.1993@gmail.com>
2017-05-12 23:11:03 +0200

    Added ViewController class.

---
+++ src/main/java/com/prodevs/controller/ViewController.java
@@ -0,0 +1,19 @@
+package com.prodevs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Created by Krzysztof on 2017-05-12.
+ */
+@Controller
+public class ViewController {
+
+    @GetMapping(value = "/")
+    public String displayWelcomePage(){
+
+        return "welcome-page";
+
+    }
+
+}
