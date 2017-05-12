f796b4ab18a4a91aecadb857164bfac5b4b42982
K0XM4N <radwan.krzysztof.1993@gmail.com>
2017-05-12 23:04:26 +0200

    Changed SpringBoot app class name.

--- src/main/java/com/prodevs/SpringPoolApp.java
+++ src/main/java/com/prodevs/SpringPoolApp.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringPoolAppApplication {
+public class SpringPoolApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringPoolAppApplication.class, args);
+		SpringApplication.run(SpringPoolApp.class, args);
 	}
 }
