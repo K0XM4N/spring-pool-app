3c6e00ccbb42a5dcd0de415bd84593d4d7f4cb71
K0XM4N <radwan.krzysztof.1993@gmail.com>
2017-05-17 23:21:45 +0200

    Added service class

---
+++ src/main/java/com/prodevs/service/DailyRaportService.java
@@ -0,0 +1,22 @@
+package com.prodevs.service;
+
+import com.prodevs.repository.DailyRaportRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Krzysztof on 2017-05-17.
+ */
+@Service
+public class DailyRaportService {
+
+    @Autowired
+    DailyRaportRepository dailyReportRepo;
+
+    public DailyRaportService(DailyRaportRepository dailyReportRepo){
+        this.dailyReportRepo = dailyReportRepo;
+    }
+
+
+
+}
