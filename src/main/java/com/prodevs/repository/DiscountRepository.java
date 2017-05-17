9fae094cab96edae9f2b5a1afbf574b5b8845ee0
K0XM4N <radwan.krzysztof.1993@gmail.com>
2017-05-17 22:55:01 +0200

    Added @Repository annotation.

--- src/main/java/com/prodevs/repository/DiscountRepository.java
+++ src/main/java/com/prodevs/repository/DiscountRepository.java
@@ -2,9 +2,11 @@ package com.prodevs.repository;
 
 import com.prodevs.model.Discount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by Krzysztof on 2017-05-17.
  */
+@Repository
 public interface DiscountRepository extends JpaRepository<Discount, Integer> {
 }
