b4a6113c82160a02de50124147415b967ecfb8cb
K0XM4N <radwan.krzysztof.1993@gmail.com>
2017-05-21 22:43:17 +0200

    Added register method

--- src/main/java/com/prodevs/controller/EmployeeController.java
+++ src/main/java/com/prodevs/controller/EmployeeController.java
@@ -1,8 +1,12 @@
 package com.prodevs.controller;
 
+import com.prodevs.model.Employee;
 import com.prodevs.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  * Created by Krzysztof on 2017-05-21.
@@ -16,4 +20,13 @@ public class EmployeeController {
     public EmployeeController(EmployeeService service){
         this.employeeService = service;
     }
+
+    @PostMapping(value = "/register")
+    public String registerEmployee(@ModelAttribute Employee registeredEmployee, Model model){
+
+        employeeService.save(registeredEmployee);
+        return "register-page";
+
+    }
+
 }
