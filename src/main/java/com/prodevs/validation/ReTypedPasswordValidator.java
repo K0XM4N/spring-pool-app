d1e0c421fbd42145fdebef96f461c6ed6b7a5270
K0XM4N <radwan.krzysztof.1993@gmail.com>
2017-08-03 20:49:50 +0200

    Validation: implementing re-typed password validators

---
+++ src/main/java/com/prodevs/validation/ReTypedPasswordValidator.java
@@ -0,0 +1,22 @@
+package com.prodevs.validation;
+
+import com.prodevs.validation.annotation.ReTypedPassword;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+/**
+ * Created by Krzysztof on 2017-08-03.
+ */
+public class ReTypedPasswordValidator implements ConstraintValidator<ReTypedPassword, String> {
+
+    @Override
+    public void initialize(ReTypedPassword reTypedPassword) {
+
+    }
+
+    @Override
+    public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
+        return false;
+    }
+}
---
+++ src/main/java/com/prodevs/validation/annotation/ReTypedPassword.java
@@ -0,0 +1,16 @@
+package com.prodevs.validation.annotation;
+
+import com.prodevs.validation.ReTypedPasswordValidator;
+
+import javax.validation.Constraint;
+import java.lang.annotation.*;
+
+/**
+ * Created by Krzysztof on 2017-08-03.
+ */
+@Documented
+@Target({ElementType.FIELD})
+@Constraint(validatedBy = ReTypedPasswordValidator.class)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ReTypedPassword {
+}
